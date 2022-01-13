c19b03d9d20f13527296e968711078db69410314
intns <84647527+intns@users.noreply.github.com>
2022-01-13 01:23:31 +0000

    Annotate testground.cpp

--- src/sysAxiotU/testground.cpp
+++ src/sysAxiotU/testground.cpp
@@ -1,23 +1,34 @@
-#include "Game/MoviePlayer.h"
 #include "Game/Navi.h"
 #include "efx/TSimple.h"
 
-namespace util {
+/*
+ * BY INTNS / AXIOT 2022
+ *
+ * This file is meant to serve as an introduction into modding Pikmin 2 via assisted decompilation.
+ *
+ * This example mod, when built, adds a particle system to Olimar that can be moved around using the C-stick.
+ *
+ * This mod may have bugs or unintended side-effects, and is only meant to be used for learning purposes.
+ */
+
+// Spawns a single particle at a set position with an effect ID, for a list of IDs go to
+// https://pikmintkb.com/wiki/Pikmin_2_particle_list
 inline void SpawnParticle_1(u16 effectID, const Vector3f& position)
 {
 	efx::TSimple1 simple_fx(effectID, nullptr);
 	efx::Arg settings(position);
 	simple_fx.create(&settings);
 }
-} // namespace util
 
 namespace mod {
+// A barebones controller structure just to find the C-stick controls
 struct CStickControls {
 	u8 _00[0x58];
 	f32 m_cStickHoriz;    // _58
 	f32 m_cStickVertical; // _5C
 };
 
+// Our manager for the particle attached to the player
 struct PlayerParticle {
 	PlayerParticle(u16 idx)
 	    : m_fxGenerator(idx, nullptr)
@@ -25,6 +36,8 @@ struct PlayerParticle {
 		m_offset.x = m_offset.y = 0;
 	}
 
+	// Clamps the position of the particle to "max_offset"
+	// distance away from the player
 	void clampOffset()
 	{
 		const float max_offset = 75;
@@ -42,6 +55,7 @@ struct PlayerParticle {
 		}
 	}
 
+	// Code that is ran each time NaviWalkState::exec runs
 	void handleFrame(CStickControls* controls)
 	{
 		const float move_speed = 3.75f;
@@ -74,6 +88,7 @@ struct PlayerParticle {
 		m_fxGenerator.forceKill();
 	}
 
+	// Our particle generator
 	efx::TSimple1 m_fxGenerator;
 
 	Vector3f m_origin;
@@ -86,18 +101,16 @@ static PlayerParticle gPlayerParticleMgr(19);
 // This function is branched to from (8017E988 - plugProjectKandoU/naviState.s - line 2560)
 void Handle_NaviWalkState(Game::Navi* navi)
 {
-	if (Game::moviePlayer->m_flags == Game::MoviePlayer::IS_ACTIVE) {
-		return;
-	}
-
 	// Hacky way of doing it - better alternative would be to fill out pad input properly
 	CStickControls* controller = (CStickControls*)navi->m_padinput;
 
-	// We'll only apply this to Olimar
+	// We'll only apply the particle to Olimar, which has m_padinput defined, this code
+	// runs for player 2 too, so if it is nullptr (!controller) then it is not Olimar
 	if (!controller) {
 		return;
 	}
 
+	// Set the origin for the particle and spawn
 	gPlayerParticleMgr.m_origin = navi->getPosition();
 	gPlayerParticleMgr.handleFrame(controller);
 }
