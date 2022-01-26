699622b649887a2607bd21d509228d795a9a9444
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-01-26 03:36:52 -0500

    funny pikmin launching with toggle

--- src/plugProjectEpochU/firstPersonWithGun.cpp
+++ src/plugProjectEpochU/firstPersonWithGun.cpp
@@ -1,6 +1,9 @@
 #include "types.h"
 #include "Epoch/firstPersonWithGun.h"
 #include "Game/FakePiki.h"
+#include "Game/Piki.h"
+#include "Game/Navi.h"
+#include "ObjectTypes.h"
 #include "Game/gameSystem.h"
 #include "SoundID.h"
 #include "PSSystem/SysIF.h"
@@ -11,8 +14,10 @@
 namespace Game {
 
 // HoudaiShotGunMgr* sNaviGunMgr;
-static int sEfxId          = 339;
-static bool isCStickCamera = true;
+static int sEfxId           = 339;
+static bool isCStickCamera  = true;
+static bool isBouncySphere  = true;
+static bool isParticleSpawn = true;
 
 void firstPersonGunCamera(PlayCamera& player_camera) // sets player_camera angle
 // hooked at 0x8023F928 in playCamera.s
@@ -96,17 +101,77 @@ void naviParticleSpawn(Game::Navi* navi, int efx_id)
 	util::SpawnParticle_1(efx_id, particle_position);
 }
 
+void naviBouncySphere(Game::Navi* navi)
+{
+	Vector3f sphere_pos = navi->getPosition();
+	
+	sphere_pos.x += (-100.0f * navi->m_padinput->cstick_lr);
+	// sphere_pos.y += 5.0f /* (100.0f * navi->m_padinput->cstickdeflection)*/;
+	sphere_pos.z += (100.0f * navi->m_padinput->cstick_up);
+	
+	// Create a sphere with a radius of 75 at olimar's position
+    Sys::Sphere detectionSphere(sphere_pos, 75);
+    Game::CellIteratorArg arg(detectionSphere);
+	
+	// Create the iterator based on the sphere
+    Game::CellIterator iterator(arg);
+	
+	// Initialise properly
+    iterator.first();
+	
+	// Loop until there are no objects left
+    while (!iterator.isDone()) {
+        // Grab the current object by calling *iterator
+        Game::CellObject* obj = *iterator;
+
+        // Push any Pikis away from Olimar - he has bad breath.
+        if (obj->getObjType() == OBJTYPE_Piki) {
+            Game::Piki* piki_obj = (Game::Piki*)obj;
+            Vector3f piki_pos    = piki_obj->getPosition();
+
+            // Turn the position into a direction (velocity), pushes the Piki away from the player
+            piki_pos.x -= sphere_pos.x;
+            piki_pos.y = 10.0f;
+            piki_pos.z -= sphere_pos.z;
+
+            // Exaggerate the velocity
+            piki_pos.x *= 25;
+            piki_pos.z *= 25;
+
+            piki_obj->setVelocity(piki_pos);
+        }
+
+        // Iterate to the next object
+        iterator.next();
+    }
+}
+
 void useNaviController(Navi* player)
 {
 	if ((player != nullptr) /* && (input != 0)*/) {
 		u32 input = (player->m_padinput->press);
 		// OSReport("input = %x\n", input); // reports input as unsigned long
-		if (player->m_padinput->cstickdeflection > 0.1f) {
+		if ((player->m_padinput->cstickdeflection > 0.1f) && isParticleSpawn) {
 			naviParticleSpawn(player, sEfxId);
 		}
-		if ((input & 0x10) != 0) // press Z (0x10) to disable C-Stick camera control
+		if ((player->m_padinput->cstickdeflection > 0.1f) && isBouncySphere) {
+			naviBouncySphere(player);
+		}
+		
+		// toggle flags and increment particle ID
+		if ((input & 0x10) != 0) // press Z (0x10) to toggle C-Stick camera control
 		{
 			isCStickCamera = !isCStickCamera;
+		}
+		if ((input & 0x1) != 0) // press D-PAD left (0x1) to toggle Bouncy Sphere
+		{
+			isBouncySphere = !isBouncySphere;
+			PSSystem::spSysIF->playSystemSe(PSSE_EV_HOME_PELLET_FINISH, 0);
+		}
+		if ((input & 0x2) != 0) // press D-PAD right (0x2) to toggle particle spawn
+		{
+			isParticleSpawn = !isParticleSpawn;
+			PSSystem::spSysIF->playSystemSe(PSSE_EV_ROCKET_LAND, 0);
 		}
 		if ((input & 0x40) != 0) // press L (0x40) to increment spawned particle ID
 		{
